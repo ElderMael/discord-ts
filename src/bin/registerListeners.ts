14eaf1107acefb6977974d1d977bb7dec047ce41
ElderMael <sftwr.mael@gmail.com>
2018-09-22 17:18:54 -0500

    Fix Error While Retrieving Random Result Being Always 0

--- src/bin/registerListeners.ts
+++ src/bin/registerListeners.ts
@@ -30,7 +30,7 @@ export function registerListenersTo(client: DiscordClient) {
             + `//Instances_Search?q=${searchTerm}&pageIndex=0&pageSize=10&apiKey=${memeGeneratorApiKey}`;
 
             axios.get(memeGeneratorUrl).then((response: AxiosResponse<any>) => {
-                const imageUrl = _.sample(response.data.result[0]).instanceImageUrl;
+                const imageUrl = _.sample(response.data.result).instanceImageUrl;
                 channelMessage.channel.send(imageUrl);
             }).catch((error) => {
                 console.log(chalk.red(`Error retrieving polygon news, ${error}`));
