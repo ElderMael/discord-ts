31314af7689d489a733ac33cbeceabe8eb4521fa
ElderMael <sftwr.mael@gmail.com>
2018-09-30 17:31:40 -0500

    Remove Very Small Function To Start Robot
    It does not pay for itself

--- src/bin/app.ts
+++ src/bin/app.ts
@@ -1,11 +1,18 @@
 import * as commander from "commander";
 import {config as configDotEnv} from "dotenv";
-import {startBot} from "./startBot";
+import {Client as DiscordClient} from "discord.js";
+import {registerListenersTo} from "./registerListeners";
 
 configDotEnv();
 
 if (process.argv.length === 2) {
-    startBot();
+    const client = new DiscordClient();
+
+    registerListenersTo(client);
+
+    client.login(process.env.BOT_TOKEN).then((result) => {
+        console.log(result);
+    });
 } else {
     // tslint:disable-next-line
     const version = require("../../package.json").version;
