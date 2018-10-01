1d4562b2e2922d8db4e1e5dd68a19a1caf65f385
ElderMael <sftwr.mael@gmail.com>
2018-09-30 22:37:05 -0500

    Log Only User Display Name

--- src/ClearMessageListener.ts
+++ src/ClearMessageListener.ts
@@ -13,11 +13,12 @@ export class ClearMessageListener implements MessageListener {
 
         const guildMember = msg.guild.member(msg.author);
 
-        if (
-            !guildMember.hasPermission("ADMINISTRATOR") ||
-            !guildMember.hasPermission("MANAGE_MESSAGES")
-        ) {
+        console.info(`Guild Member '${guildMember.displayName}' tries to delete ${howMany} messages.`);
+
+        if (!guildMember.hasPermission("ADMINISTRATOR")) {
+
             msg.author.send("clearing messages is prohibited for you!");
+
             return Promise.reject(new Error(`User '${msg.author.username}' attempted to clear \
                 messages on channel ${msg.channel.toString()}`));
         }
@@ -25,11 +26,6 @@ export class ClearMessageListener implements MessageListener {
         return msg.channel.fetchMessages({limit: howMany})
             .then((messages: Collection<Snowflake, Message>) => {
                 return messages.map((message) => message.delete());
-            }).then((deletedMessages) => {
-                const roles = guildMember.roles.map((role) => role.name);
-                console.info(roles);
-                console.info(`User ${msg.author.username} (${roles}) removed ${howMany} messages!`);
-                return deletedMessages;
             });
     }
 
