2c67dc073798a75558b1323feaab48ff53f9476e
ElderMael <sftwr.mael@gmail.com>
2018-10-12 17:20:58 -0500

    Find Role Using Matching

--- src/listeners/AddRoleMessageListener.ts
+++ src/listeners/AddRoleMessageListener.ts
@@ -14,7 +14,7 @@ export class AddRoleMessageListener implements MessageListener {
             return Promise.reject(`Invalid command: ${msg.content} `);
         }
 
-        const guildRole = msg.guild.roles.find('name', role);
+        const guildRole = msg.guild.roles.filter(r => r.name.includes(role));
 
 
         if (!guildRole) {
