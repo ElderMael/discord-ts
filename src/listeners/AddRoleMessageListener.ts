2412bce8e0a9cf4b20fe1678fbcad250d08383ae
ElderMael <sftwr.mael@gmail.com>
2018-10-12 17:57:03 -0500

    Add Admin Check And Format

--- src/listeners/AddRoleMessageListener.ts
+++ src/listeners/AddRoleMessageListener.ts
@@ -2,11 +2,20 @@ import {Message} from "discord.js";
 import MessageListener from "./MessageListener";
 
 export class AddRoleMessageListener implements MessageListener {
+
     canProcess(msg: Message): boolean {
         return msg.content.startsWith('!add-role');
     }
 
     process(msg: Message): Promise<any> {
+
+        const messageAuthorDoesNotHaveAdminAccess = !msg.guild
+            .member(msg.author)
+            .hasPermission('ADMINISTRATOR');
+
+        if (messageAuthorDoesNotHaveAdminAccess) {
+            return msg.channel.send(`You do not have rights for this!`);
+        }
 
         const [command, user, role] = msg.content.split(' ');
 
@@ -14,14 +23,13 @@ export class AddRoleMessageListener implements MessageListener {
             return Promise.reject(`Invalid command: ${msg.content} `);
         }
 
-        const guildRole = msg.guild.roles.filter(r => r.name.includes(role)).first();
-
+        const guildRole = msg.guild.roles
+            .filter(r => r.name.includes(role))
+            .first();
 
         if (!guildRole) {
-            const roles = msg.guild.roles
-                .map(r => r.name)
-                .join(',');
-            return msg.channel.send(`Role not found ${role}, valid roles are: ${roles}`);
+            return msg.channel
+                .send(`Role not found ${role}.`);
         }
 
 
@@ -31,20 +39,18 @@ export class AddRoleMessageListener implements MessageListener {
             return msg.channel.send(`User does not exist ${user}`);
         }
 
-        let guildMember = msg.guild
-            .member(guildUser);
-
-        if (!guildMember.hasPermission("ADMINISTRATOR")) {
-            return msg.channel.send(`You do not have rights for this!`);
-        }
+        let guildMember = msg.guild.member(guildUser);
 
         return guildMember
             .addRole(guildRole)
             .then(() => {
-                return msg.channel.send(`Added role ${role} to user ${user}`);
-            }).catch(error => {
+                return msg.channel
+                    .send(`Added role ${role} to user ${user}`);
+            })
+            .catch(error => {
                 console.log(error);
-                return msg.channel.send(`Error adding role ${role} to user ${user}`)
+                return msg.channel
+                    .send(`Error adding role ${role} to user ${user}`)
             });
     }
 
