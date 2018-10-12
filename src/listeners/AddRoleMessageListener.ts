344215ae67328221691b072a868697c8d2050cc9
ElderMael <sftwr.mael@gmail.com>
2018-10-12 14:57:08 -0500

    Handle Better Add Role Errors

--- src/listeners/AddRoleMessageListener.ts
+++ src/listeners/AddRoleMessageListener.ts
@@ -11,7 +11,17 @@ export class AddRoleMessageListener implements MessageListener {
         const [command, user, role] = msg.content.split(' ');
 
         if (command && user && role) {
-            return msg.guild.member(user).addRole(role);
+            const guildRole = msg.guild.roles.find('name', role);
+
+            if (!guildRole) {
+                return msg.channel.send(`Role not found ${guildRole}`);
+            }
+
+            return msg.guild.member(user).addRole(guildRole).then(() => {
+                return msg.channel.send(`Added role ${role} to user ${user}`);
+            }).catch(error => {
+                return msg.channel.send(`Error adding role ${role} to user ${user}`)
+            });
         }
 
         return Promise.reject(`Invalid command: ${msg.content} `);
