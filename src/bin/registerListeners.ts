439a6d5b2382b6666e09653bc92ebfb77639f9cc
ElderMael <sftwr.mael@gmail.com>
2018-11-05 20:36:35 -0600

    Process Results Differently

--- src/bin/registerListeners.ts
+++ src/bin/registerListeners.ts
@@ -1,22 +1,20 @@
 import {Message} from "discord.js";
 import {Client as DiscordClient} from "discord.js";
 import {listeners} from "../listeners/MessageListener";
+import * as _ from "lodash";
 
 export function registerListenersTo(client: DiscordClient) {
 
     client.on("message", (message: Message) => {
 
-        listeners
-            .filter(listener => listener.canProcess(message))
-            .forEach(listener => {
-                listener
-                    .process(message)
-                    .catch(error => {
-                        console.error(`Error while processing message '${message.content}':`, error);
-                        return message.channel.send("Ups! Error 😞");
-                    });
-            });
+        const processors = _.filter(listeners, listener => listener.canProcess(message));
 
+        const processorResults = _.map(processors, listener => listener.process(message));
+
+        _.forEach(processorResults, result => result.catch(error => {
+            console.error(`Error while processing message '${message.content}':`, error);
+            return message.channel.send("Ups! Error 😞");
+        }));
 
     });
 }
